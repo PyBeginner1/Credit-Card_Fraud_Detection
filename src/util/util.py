69815f4eb2a1a0c9d3f967a7beea5583de0ea7c6
PyBeginner1 <shashvathn@gmail.com>
2023-05-03 10:16:56 +0530

    Helper functions created

--- src/util/util.py
+++ src/util/util.py
@@ -1,6 +1,6 @@
 import os,sys
 import yaml
-
+import dill
 
 from src.logger import logging
 from src.exception import FraudException
@@ -18,5 +18,15 @@ def read_yaml_file(file_path:str)->dict:
         logging.info(e)
         raise FraudException(e,sys) from e
 
+
+def save_object(file_path:str,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        logging.info(e)
+        raise FraudException(e,sys) from e 
 
 
