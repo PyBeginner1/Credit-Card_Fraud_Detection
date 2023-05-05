ed1354e40f2e0a32848b654530ff60c6cc24fe51
PyBeginner1 <shashvathn@gmail.com>
2023-05-05 09:31:10 +0530

    Helper functions added for Model evaluation

--- src/util/util.py
+++ src/util/util.py
@@ -40,5 +40,33 @@ def load_object(file_path:str):
             return dill.load(file_obj)
     except Exception as e:
         raise FraudException(e,sys) from e
+    
+
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise FraudException(e,sys) from e
+    
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads yaml file and returns dictionary
+    """
+    try:
+        with open(file_path,'r') as yaml_file:
+            logging.info('Reading Yaml file')
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        logging.info(e)
+        raise FraudException(e,sys) from e
 
 
